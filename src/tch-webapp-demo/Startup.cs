c20702afb395daff6e47de3f1b97f85c2f9a5312
Mohammed El-Nabulsi <Mohammed.El-Nabulsi@outlook.de>
2016-11-26 04:48:29 +0100

    Added Frontend, Mvc and Dependencies

--- src/tch-webapp-demo/Startup.cs
+++ src/tch-webapp-demo/Startup.cs
@@ -5,10 +5,11 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 
-namespace tch_webapp_demo
+namespace WebAppDemo
 {
     public class Startup
     {
@@ -16,6 +17,7 @@ namespace tch_webapp_demo
         // For more information on how to configure your application, visit http://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+			services.AddMvc();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -28,10 +30,8 @@ namespace tch_webapp_demo
                 app.UseDeveloperExceptionPage();
             }
 
-            app.Run(async (context) =>
-            {
-                await context.Response.WriteAsync("Hello World!");
-            });
+			app.UseStaticFiles();
+			app.UseMvcWithDefaultRoute();
         }
     }
 }
